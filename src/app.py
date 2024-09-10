39a49f59e75a20e884612284f795316f9d17818b
PatriciaAlEs <168550797+PatriciaAlEs@users.noreply.github.com>
2024-09-10 11:13:44 +0000

    4 errores

--- src/app.py
+++ src/app.py
@@ -8,8 +8,11 @@ from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
 #from models import Person
 
+# Se inicia la aplicacion de flask
 app = Flask(__name__)
+# Cosas standar
 app.url_map.strict_slashes = False
+# Metodo de cross origin referrer security
 CORS(app)
 
 # create the jackson family object
@@ -25,18 +28,60 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
+
+# ENDPOINTS 
+# GET | POST | PUT | DELETE | PATCH
+
 @app.route('/members', methods=['GET'])
 def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
+    response_body = [ {
         "family": members
-    }
-
-
+    }]
+    # devolvemos el END POINT jsonify(nuestra_informacion), codigo status
+    # 404 - Not found
+    # 200 - ok
+    # 400 - son errores 
+    # 500 - error del servidor (nosotros somos el servidor)
     return jsonify(response_body), 200
+
+@app.route('/member', methods=['POST'])  # agregar informacion
+def add_member():
+    new_member = request.json
+    print(new_member)
+
+    jackson_family.add_member(new_member)
+    return jsonify({"done" : "usuario creado JEJEJEJE"})
+
+
+# @app.route('/members', methods=['PUT'])
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_family_member(member_id):
+    eliminar_familiar = jackson_family.delete_member(member_id)
+    if not eliminar_familiar:
+        return jsonify({"familiar not found"}),400
+    return jsonify({"hecho": "el borrado funcionó"}), 200
+
+@app.route('/member/<int:member_id>', methods=['PUT'])
+def update_family_member(member_id):
+    new_member = request.json
+    print(member_id)
+    update_member_id = jackson_family.update_member(member_id, new_member)
+    if not update_member_id:
+        return jsonify({"msg", "no se encontró el miembro"}), 400
+    return jsonify({"donde": " se actualizó"})
+
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_one_member(member_id):
+    miembro_encontrado = jackson_family.get_member(member_id)
+    if not miembro_encontrado:
+        return jsonify({"msg":"no se encontró al miembro"})
+    return jsonify(miembro_encontrado), 200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -1,36 +1,105 @@
 
 """
-update this file to implement the following already declared methods:
-- add_member: Should add a member to the self._members list
-- delete_member: Should delete a member from the self._members list
-- update_member: Should update a member from the self._members list
-- get_member: Should return a member from the self._members list
+actualice este archivo para implementar los siguientes métodos ya declarados:
+- add_member: debe agregar un miembro a la lista self._members
+- eliminar_miembro: debe eliminar un miembro de la lista self._members
+- update_member: debe actualizar un miembro de la lista self._members
+- get_member: debe devolver un miembro de la lista self._members
+"""
+
+"""
+DICCIONARIO, VALORES
++ id: Int
++ first_name: String
++ last_name: String (Siempre Jackson)
++ age: Int > 0
++ lucky_numbers: List of integers
 """
 from random import randint
 
 class FamilyStructure:
+
     def __init__(self, last_name):
         self.last_name = last_name
+        self._next_id = 1
+        self._members = [
+        {
+            "id": self._generate_id(), 
+            "first_name": "John", 
+            "last_name": self.last_name, 
+            "age": "33 years old",
+            "lucky_numbers" : [7, 13, 22]   
+        },
 
-        # example list of members
-        self._members = []
+       {
+            "id": self._generate_id(),
+            "first_name": "Jane", 
+            "last_name": self.last_name, 
+            "age": "35 years old",
+            "lucky_numbers" : [10, 14, 3]
+        },
 
-    # read-only: Use this method to generate random members ID's when adding members into the list
+         {
+            "id": self._generate_id(), 
+            "first_name": "Jimmy", 
+            "last_name": self.last_name, 
+            "age": "5 years old",
+            "lucky_numbers" : [1]
+        }
+                    ]
+
+
+# Este método genera un 'id' único al agregar miembros a la lista (no debes modificar esta función)
+    def _generate_id(self):
+        generated_id = self._next_id
+        self._next_id += 1
+        return generated_id
+    
+# solo lectura: use este método para generar ID de miembros aleatorios al agregar miembros a la lista
     def _generateId(self):
         return randint(0, 99999999)
 
+
     def add_member(self, member):
-        # fill this method and update the return
+        if not member.get("id"):
+            member['id'] = self._generate_id()
+        self._members.append(member)
         pass
+
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        ## Recorre la lista y elimina el miembro con el id proporcionado
+        for member in self._members:   # itera (recorre) en los diccionarios de cada miembro
+            if member['id'] == id:     #compara el id del diccionario (member['id'] con el id al que hacemos referencia en el parametro)
+                self._members.remove(member)
+                return True
+                # sale del for porque ya encontró y no tiene sentido seguir buscando
+        return print(f"No existe el miembro con el id {id}")
+        #fuera del bucle se imprime el mensaje despues de haber revisado todos los miembros y no haber encontrado ninguno 
+    pass
 
     def get_member(self, id):
-        # fill this method and update the return
+        for family_member in self._members:
+            if family_member['id'] == id:
+                return family_member
+            
+        return False         # Devuelve el diccionario del miembro encontrado
         pass
 
-    # this method is done, it returns a list with all the family members
+    def update_member(self, id, member):
+        print("Actualizando", id)
+        for family_member in self._members:
+            if family_member['id'] == id:
+                self._members.remove(family_member)
+                member["id"] = id
+                self._members.append(member)
+                return True
+
+        return False 
+  
+    
+
+
+    # este método está hecho, devuelve una lista con todos los miembros de la familia
     def get_all_members(self):
         return self._members
